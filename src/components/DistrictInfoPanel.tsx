6821dc8239088c07bc10e15a5461b9539e918fde
fal4782 <falguni4782@gmail.com>
2025-09-08 12:51:49 +0530

    district panel padding fix

--- src/components/DistrictInfoPanel.tsx
+++ src/components/DistrictInfoPanel.tsx
@@ -127,7 +127,7 @@ const DistrictInfoPanel = ({
               </div>
             </div>
           ) : (
-            <div className="text-center py-12">
+            <div className="text-center py-2">
               <div className="w-20 h-20 mx-auto mb-6 bg-gradient-to-br from-yellow-100 to-orange-200 rounded-full flex items-center justify-center">
                 <Package className="w-10 h-10 text-orange-600" />
               </div>
